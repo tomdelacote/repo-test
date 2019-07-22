2e74198781f2b156b94e79d76567463fbece1238
Matt <52179069+Rapciu@users.noreply.github.com>
2019-07-22 14:13:03 +0200

    Added the main code.

--- word_generator.py
+++ word_generator.py
@@ -1,1 +1,14 @@
+import string
+import random
+
 print("Word Generator")
+
+alphabet = string.ascii_lowercase
+
+word = ""
+
+for i in range(random.randint(3, 8)):
+    word += random.choice(alphabet)
+    
+print(word)
+
